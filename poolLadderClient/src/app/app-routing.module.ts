c717d3ed3922e984db5ea5f4cb142bad6be1b18f
Ashley Stewart <ashley.stewart@equinox.co.nz>
2019-05-03 16:16:07 +1200

    flexbox styles added

--- poolLadderClient/src/app/app-routing.module.ts
+++ poolLadderClient/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'add-user', component: AddUserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- poolLadderClient/src/app/app.module.ts
+++ poolLadderClient/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AddUserComponent } from './components/add-user/add-user.component';
 import { ScheduleGameComponent } from './components/schedule-game/schedule-game.component';
+
+import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material/material.module';
 
 @NgModule({
@@ -24,7 +26,8 @@ import { MaterialModule } from './material/material.module';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
